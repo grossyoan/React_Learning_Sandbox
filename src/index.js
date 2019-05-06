71b90018afdf980d188ba9b98661f8fe21089fa4
gross.yoan@gmail.com <32864409+grossyoan@users.noreply.github.com>
2019-05-06 18:21:36 +0200

    :construction: Working on components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// function tickUpdateTest() {
+//     const element = (
+//         <div>
+//         <h1> Hello there!</h1>
+//         <h2> Today's date is {new Date().toLocaleDateString()}, and it's {new Date().toLocaleTimeString()}</h2>
+//         </div> 
+//     )
+//     ReactDOM.render(element, document.getElementById('root'))
+// }
+
+// tickUpdateTest(autoUpdate, 1000);
+
+function Welcome(props) {
+    return <h1>Welcome {props.name}, we're happy to have you back!</h1>
+}
+
+const element = <Welcome name="Yoan" />
+
+ReactDOM.render(
+    element,
+    document.querySelector('#root')
+)
+
